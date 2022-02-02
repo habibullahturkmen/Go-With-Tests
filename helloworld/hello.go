f0a2acf5f33dba1949bd65cd399bedf8da30efa3
habibullahturkmen <habibullah_turkmen@yahoo.con>
2022-02-02 15:51:55 +0430

    hello world test completed

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
+const turkish = "Turkish"
+const french = "French"
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const turkishHelloPrefix = "Selam, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case turkish:
+		prefix = turkishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -12,20 +12,32 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Habib")
+		got := Hello("Habib", "English")
 		want := "Hello, Habib"
-
-		if got != want {
-			assertCorrectMessage(t, got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want:= "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 
-		if got != want {
-			assertCorrectMessage(t, got, want)
-		}
+	t.Run("in Turkish", func(t *testing.T) {
+		got := Hello("Ahmet", "Turkish")
+		want := "Selam, Ahmet"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Chris", "French")
+		want := "Bonjour, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Adam", "Spanish")
+		want := "Hola, Adam"
+		assertCorrectMessage(t, got, want)
 	})
 }

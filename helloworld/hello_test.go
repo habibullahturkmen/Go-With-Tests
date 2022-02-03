e96531228562f45ebf7724c5893d6999ffa96879
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-02-03 20:39:40 +0430

    refactor: table test

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -3,41 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-
-	assertCorrectMessage := func(t testing.TB, got, want string) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+	type args struct {
+		name     string
+		language string
 	}
-
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Habib", "English")
-		want := "Hello, Habib"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("", "English")
-		want:= "Hello, world"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("in Turkish", func(t *testing.T) {
-		got := Hello("Ahmet", "Turkish")
-		want := "Selam, Ahmet"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("in French", func(t *testing.T) {
-		got := Hello("Chris", "French")
-		want := "Bonjour, Chris"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("in Spanish", func(t *testing.T) {
-		got := Hello("Adam", "Spanish")
-		want := "Hola, Adam"
-		assertCorrectMessage(t, got, want)
-	})
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"saying hello to people", args{"Habib", "English"}, "Hello, Habib"},
+		{"say 'Hello, World' when an empty string is supplied", args{"", "English"}, "Hello, world"},
+		{"in Turkish", args{"Ahmet", "Turkish"}, "Selam, Ahmet"},
+		{"in French", args{"Chris", "French"}, "Bonjour, Chris"},
+		{"in Spanish", args{"Adam", "Spanish"}, "Hola, Adam"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if got := Hello(test.args.name, test.args.language); got != test.want {
+				t.Errorf("got %q want %q", got, test.want)
+			}
+		})
+	}
 }

a46e3664e35c0ef617624b7550d688bc9ef0f6cc
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-02-11 02:41:14 +0430

    in_memory_store and integration_tests added

--- web_application/main.go
+++ web_application/main.go
@@ -5,24 +5,8 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct {}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-		if name == "Pepper" {
-			return  20
-		}
-
-		if name == "Floyd" {
-			return 10
-		}
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-
-}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
-	log.Fatal(http.ListenAndServe(":5050", server))
+	store := NewInMemoryPlayerStore()
+	server := &PlayerServer{store}
+	log.Fatal(http.ListenAndServe(":5000", server))
 }
